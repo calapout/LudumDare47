427994ea47b48d71e7e523831e972b9d4a8d9bc7
Jimmy Tremblay-Bernier <calapout@gmail.com>
2020-10-02 22:22:37 -0400

    implemented the Entity and updated the README

---
+++ Assets/Scripts/EntityData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Bytes;
+
+public class EntityData : Bytes.Data
+{
+    public Entity entity;
+
+    public EntityData(Entity entity)
+    {
+        this.entity = entity;
+    }
+}
