f99e6f186e05d86b62ceadf9a0f7e55b5bd32606
Jimmy Tremblay-Bernier <calapout@gmail.com>
2020-10-04 15:52:36 -0400

    added the GameTimer

---
+++ Assets/Scripts/GameTimerData.cs
@@ -0,0 +1,13 @@
+﻿using Bytes;
+public class GameTimerData : Bytes.Data
+{
+    public float pourcent
+    {
+        get => pourcent;
+        private set => pourcent = value;
+    }
+    public GameTimerData(float pourcent)
+    {
+        this.pourcent = pourcent;
+    }
+}
